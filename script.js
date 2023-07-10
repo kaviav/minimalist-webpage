c23ee8c4010a7bfb8a38f9d2ce23c087b20043ba
Whale77777 <134940952+Whale77777@users.noreply.github.com>
2023-07-10 19:22:39 +0530

    updated

--- script.js
+++ script.js
@@ -58,3 +58,14 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  clicked.classList.add("operations__tab--active");
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
