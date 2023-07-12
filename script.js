1b417513f6fa39e45cb921a78412adac14de3c52
Whale77777 <134940952+Whale77777@users.noreply.github.com>
2023-07-12 23:20:32 +0530

    files added

--- script.js
+++ script.js
@@ -82,3 +82,15 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
